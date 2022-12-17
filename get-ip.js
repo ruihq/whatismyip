870a811b87eb29e6482d10ed815fda1d6f896763
Kylee ;) <120704613+kyleearroyo@users.noreply.github.com>
2022-12-16 20:48:05 -0800

    * UPDATE Added IPV6 Support And Report Button!

--- get-ip.js
+++ get-ip.js
@@ -1,10 +1,34 @@
-// JavaScript file (get-ip.js)
+// Create an async function to retrieve the user's IP addresses
+async function getIPAddresses() {
+  // Show the loading screen
+  document.getElementById("loading").style.display = "block";
 
-// Call an API to get the user's IP address
-fetch("https://api.ipify.org?format=json")
-  .then(response => response.json())
-  .then(data => {
-    // Update the page with the IP address
-    document.getElementById("ip-address").innerHTML = data.ip;
-  });
+  // Call the ipapi API to get the user's IPv4 address
+  let ipv4Address = "Error retrieving IPv4 address";
+  try {
+    const response1 = await fetch("https://api.ipify.org?format=json");
+    const data1 = await response1.json();
+    ipv4Address = data1.ip;
+  } catch (error) {
+    console.error(error);
+  }
+  document.getElementById("ipv4-address").innerHTML = ipv4Address;
+
+  // Call the api6.ipify.org API to get the user's IPv6 address
+  let ipv6Address = "Error retrieving IPv6 address";
+  try {
+    const response2 = await fetch("https://ipapi.co/json/");
+    const data2 = await response2.json();
+    ipv6Address = data2.ip;
+  } catch (error) {
+    console.error(error);
+  }
+  document.getElementById("ipv6-address").innerHTML = ipv6Address;
+
+  // Hide the loading screen
+  document.getElementById("loading").style.display = "none";
+}
+
+// Call the async function
+getIPAddresses();
 
