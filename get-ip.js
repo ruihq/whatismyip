f684203834b4717f0a0f3bea8087995a351745df
Rui Naigel Reogo <120704613+ruinaigelreogo@users.noreply.github.com>
2022-12-17 03:12:45 -0800

    get-ip.js:4 Uncaught SyntaxError: await is only valid in async functions and the top level bodies of modules

--- get-ip.js
+++ get-ip.js
@@ -1,22 +1,34 @@
-// Call the ipapi API to get the user's IPv4 address
-let ipv4Address = "Error retrieving IPv4 address";
-try {
-  const response1 = await fetch("https://ipapi.co/json/");
-  const data1 = await response1.json();
-  ipv4Address = data1.ip;
-} catch (error) {
-  console.error(error);
-}
-document.getElementById("ipv4-address").innerHTML = ipv4Address;
+// Create an async function to retrieve the user's IP addresses
+async function getIPAddresses() {
+  // Show the loading screen
+  document.getElementById("loading").style.display = "block";
 
-// Call the ipapi API to get the user's IPv6 address
-let ipv6Address = "Error retrieving IPv6 address";
-try {
-  const response2 = await fetch("https://ipapi.co/json/");
-  const data2 = await response2.json();
-  ipv6Address = data2.ipv6;
-} catch (error) {
-  console.error(error);
+  // Call the ipapi API to get the user's IPv4 address
+  let ipv4Address = "Error retrieving IPv4 address";
+  try {
+    const response1 = await fetch("https://ipapi.co/json/");
+    const data1 = await response1.json();
+    ipv4Address = data1.ip;
+  } catch (error) {
+    console.error(error);
+  }
+  document.getElementById("ipv4-address").innerHTML = ipv4Address;
+
+  // Call the ipapi API to get the user's IPv6 address
+  let ipv6Address = "Error retrieving IPv6 address";
+  try {
+    const response2 = await fetch("https://ipapi.co/json/");
+    const data2 = await response2.json();
+    ipv6Address = data2.ipv6;
+  } catch (error) {
+    console.error(error);
+  }
+  document.getElementById("ipv6-address").innerHTML = ipv6Address;
+
+  // Hide the loading screen
+  document.getElementById("loading").style.display = "none";
 }
-document.getElementById("ipv6-address").innerHTML = ipv6Address;
+
+// Call the async function
+getIPAddresses();
 
